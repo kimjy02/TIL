c3dd11e61e3c2d20fdd1bd2253093c079ff64c13
김주연 <02kimjy@naver.com>
2025-07-25 11:14:02 +0900

    git 삭제

---
+++ project/01-pjt/examples/03_csv_read.py
@@ -0,0 +1,16 @@
+import csv
+
+
+# with open('users.csv', 'r', encoding='utf-8') as file:
+#     # content = file.read()
+#     # print(content)
+#     csv_reader = csv.reader(file)
+#     # print(csv_reader)
+#     for row in csv_reader:
+#         print(row)
+
+with open('users.csv', 'r', encoding='utf-8') as file:
+    csv_reader = csv.DictReader(file)
+    print(csv_reader.fieldnames) # ['이름', '나이', '직업']
+    for row in csv_reader:
+        print(row)
