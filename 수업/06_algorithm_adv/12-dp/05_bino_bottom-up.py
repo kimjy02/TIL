4176939996eeab074dcaf39250d4fa7fb5db1092
김주연 <02kimjy@naver.com>
2025-08-26 08:28:21 +0900

    0825 수업 완료

---
+++ 수업/06_algorithm_adv/12-dp/05_bino_bottom-up.py
@@ -0,0 +1,20 @@
+def bino(n, k):
+    dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+    '''
+        [1, 0, 0, 0, 0]
+        [1, 1, 0, 0, 0]
+        [1, 2, 1, 0, 0]
+        [1, 3, 3, 1, 0]
+        [1, 4, 6, 4, 1]
+    '''
+    for i in range(n+1):
+        for j in range(min(i, k) + 1):
+            if j == 0 or i == j:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1]
+    return dp[n][k]
+
+n = 2
+k = 2
+print(bino(n, k))  # 출력: 10
