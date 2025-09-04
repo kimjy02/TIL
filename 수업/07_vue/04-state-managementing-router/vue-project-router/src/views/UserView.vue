24945250cb6a10d2838cbcdfbc23efb673c25613
김주연 <02kimjy@naver.com>
2025-09-04 17:38:56 +0900

    0904 수업 완료

---
+++ 수업/07_vue/04-state-managementing-router/vue-project-router/src/views/UserView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>User</h1>
+    <p>{{  route.params.username }}</p>
+    <button @click='goHome'>홈으로!</button>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+
+const router = useRouter()
+
+
+const goHome = function() {
+    router.push({ name: 'home' })
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ 수업/07_vue/04-state-managementing-router/vue-project/src/stores/articles.js
@@ -0,0 +1,8 @@
+import { ref, computed } from 'vue'
+import { defineStore} from 'pinia'
+
+export const useArticleStore = defineStore('article', () => {
+    const article = ref([])
+
+    return { article }
+})
