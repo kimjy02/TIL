5d9704547b8a1794868875b3e7ca482fe0e41fa7
김주연 <02kimjy@naver.com>
2025-09-05 13:39:20 +0900

    0905 수업 완료

---
+++ project/03-pjt/examples/vue-project/src/components/ArticleList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>Article List</h3>
+    <!-- <p>{{ articleStore }}</p> -->
+    <ArticleListItem 
+      v-for="article in articleStore.articles"
+      :key="article.id"
+      :article="article"
+    />
+  </div>
+</template>
+
+<script setup>
+import ArticleListItem from '@/components/ArticleListItem.vue'
+import { useArticleStore } from '@/stores/article';
+
+const articleStore = useArticleStore()
+</script>
