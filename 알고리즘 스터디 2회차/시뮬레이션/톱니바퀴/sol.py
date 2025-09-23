645bb5e7c0e6df6d611e79b1314a854045e05139
김주연 <02kimjy@naver.com>
2025-09-24 02:13:47 +0900

    알고리즘 스터디 시뮬레이션

--- 알고리즘 스터디 2회차/시뮬레이션/톱니바퀴/sol.py
+++ 알고리즘 스터디 2회차/시뮬레이션/톱니바퀴/sol.py
@@ -2,13 +2,24 @@
 from collections import deque
 
 def rotate_function(position, rotate):
-    if position != 3 and cogwheels[position][2] != cogwheels[position + 1][-2]:
-        cogwheels[position+1].rotate(-rotate)
+    if position != 3 and cogwheels[position][2] != cogwheels[position + 1][-2] and visited[position + 1] == False:
+        cogwheels[position].rotate(rotate)
+        visited[position] = True
+        rotate_function(position + 1, -rotate)
 
-    if position != 0 and cogwheels[position][-2] != cogwheels[position - 1][2]:
-        cogwheels[position-1].rotate(-rotate)
+        cogwheels[position].rotate(-rotate)
+        visited[position] = False
+
+    if position != 0 and cogwheels[position][-2] != cogwheels[position - 1][2] and visited[position - 1] == False:
+        cogwheels[position].rotate(rotate)
+        visited[position] = True
+        rotate_function(position - 1, -rotate)
+
+        cogwheels[position].rotate(-rotate)
+        visited[position] = False
 
     cogwheels[position].rotate(rotate)
+    visited[position] = True
 
 cogwheels = []
 for _ in range(4):
@@ -20,10 +31,12 @@ rotate_method = [list(map(int, input().split())) for _ in range(rotate_count)]
 
 for i in range(rotate_count):
     position, rotate = rotate_method[i]
+    visited = [False] * 4
     rotate_function(position-1, rotate)
 
-for i in range(4):
-    print(cogwheels[i])
+    # for j in range(4):
+    #     print(cogwheels[j])
+
 score = 0
 for i in range(4):
     if cogwheels[i][0] == 0:
