679b7e148c56ae17b3dc09b1d5ab0c6bd2279cf1
김주연 <02kimjy@naver.com>
2025-09-26 01:02:33 +0900

    알고리즘 스터디 그리디 완료

--- 알고리즘 스터디 2회차/그리디/공주님의 정원/sol.py
+++ 알고리즘 스터디 2회차/그리디/공주님의 정원/sol.py
@@ -1,10 +1,34 @@
 N = int(input())
 
 flowers = []
+
 for _ in range(N):
-    flowers.append(list(map(int, input().split())))
+    ms, ds, me, de = map(int, input().split())
+    start = ms * 100 + ds
+    end = me * 100 + de
+    flowers.append((start, end))
 
-# print(flower)
+flowers.sort(key=lambda x: (x[0], -x[1]))
 
-for flower in flowers:
-    if flower[0] == 3 and flower[1] == 1:
+current = 301
+final = 1201
+i = 0
+cnt = 0
+n = len(flowers)
+
+while current < final:
+    max_end = current
+
+    while i < n and flowers[i][0] <= current:
+        if flowers[i][1] > max_end:
+            max_end = flowers[i][1]
+        i += 1
+
+    if max_end == current:
+        print(0)
+        break
+
+    cnt += 1
+    current = max_end
+else:
+    print(cnt)
