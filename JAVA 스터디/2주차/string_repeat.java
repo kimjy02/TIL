05fd54d285fa295f444e4cda3d4b10746823b3bf
김주연 <02kimjy@naver.com>
2025-11-20 17:58:13 +0900

    알고리즘 스터디 문제 하나 풀기

--- JAVA 스터디/2주차/string_repeat.java
+++ JAVA 스터디/2주차/string_repeat.java
@@ -1,3 +1,5 @@
 public class string_repeat {
-  
+  public static void main(String[] args) {
+    
+  }
 }
--- JAVA 스터디/2주차/word_study.java
+++ JAVA 스터디/2주차/word_study.java
@@ -1,8 +1,33 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class word_study {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    
+    String s = sc.nextLine();
+    List<Character> list = new ArrayList<>();
+    Map<Character, Integer> count = new HashMap<>();
+
+    for (char c : s.toCharArray()) {
+      c = Character.toUpperCase(c);
+      list.add(c);
+
+      count.put(c, count.getOrDefault(c, 0) + 1);
+    }
+
+    int maxValue = 0;
+    char answer = '?';
+
+    for (char key : count.keySet()) {
+      int value = count.get(key);
+
+      if (value > maxValue) {
+        maxValue = value;
+        answer = key;
+      } else if (value == maxValue) {
+        answer = '?';
+      }
+    }
+
+    System.out.println(answer);
   }
 }
